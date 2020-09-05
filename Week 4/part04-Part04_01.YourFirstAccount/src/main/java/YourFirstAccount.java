9abb48910de0208d7093710414ff68bd1456739c
Threes <43138471+3threes3@users.noreply.github.com>
2020-09-05 20:35:17 +0200

    Add files via upload

---
+++ Week 4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account myMoney = new Account ("DF", 100.0); 
+        myMoney.deposit(20.0);
+        
+        System.out.println(myMoney);
+    }
+}
